bfb56b16cddd6139df916007b2eebeaf831acad4
kinjal varshney <kinjal.varshney_cs19@gla.ac.in>
2020-08-29 22:29:32 +0530

    add fields for the Book.java class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,14 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: kinjal varshney
+ *  Date: 29/08/20
+ *  Time: 9:18 PM
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
+    private String bookName;
+    private String authorName;
+    private String isbnBookNumber;
 }
+
